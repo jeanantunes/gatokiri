28f4896035a88dad1420c4d8cafb65938ca58c0b
Jean Antunes <jean.antunes@gmail.com>
2018-02-27 17:08:00 -0300

    V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS 'USER' (
-  'id' int NOT NULL DEFAULT NEXTVAL ('USER_seq'),
-  'name' varchar(50) NOT NULL,
-  'email' varchar(50) NOT NULL,
-  PRIMARY KEY ('id'),
-  CONSTRAINT UK_email UNIQUE ('email')
+CREATE TABLE IF NOT EXISTS USER (
+  id int NOT NULL DEFAULT NEXTVAL ('USER_seq'),
+  name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT UK_email UNIQUE (email)
 ) ;
