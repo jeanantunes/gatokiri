e87e14694fae36aca4caab13364735649138a92f
Jean Antunes <jean.antunes@gmail.com>
2018-02-27 16:38:57 -0300

    MySql to Postgresql V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,9 @@
+CREATE SEQUENCE USER_seq;
+
 CREATE TABLE IF NOT EXISTS USER (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL DEFAULT NEXTVAL ('USER_seq'),
   name varchar(50) NOT NULL,
   email varchar(50) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY UK_email (email)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT UK_email UNIQUE (email)
+) ;
